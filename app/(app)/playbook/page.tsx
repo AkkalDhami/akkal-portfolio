50ca4612046818b70d29fd72c2c0b5cacea2e73a
AkkalDhami <dhamiakkal21@gmail.com>
2026-03-29 08:06:19 +0545

    feat: initialize project structure with global CSS, core UI components, and modular page layouts

--- app/(app)/playbook/page.tsx
+++ app/(app)/playbook/page.tsx
@@ -1,4 +1,5 @@
 import { PlaybookSection } from "@/components/playbook/playbook-section";
+import { cn } from "@/lib/utils";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -9,7 +10,11 @@ export const metadata: Metadata = {
 
 export default function Page() {
   return (
-    <div className="border-edge mx-auto max-w-4xl border-x pt-16 pb-2">
+    <div
+      className={cn(
+        "border-edge mx-auto max-w-4xl border-x pt-16"
+        // "bg-[radial-gradient(35%_128px_at_100%_0%,--theme(--color-foreground/.05),transparent)] dark:bg-[radial-gradient(35%_128px_at_100%_0%,--theme(--color-foreground/.08),transparent),radial-gradient(35%_128px_at_0%_100%,--theme(--color-foreground/.08),transparent)]"
+      )}>
       <PlaybookSection />
     </div>
   );
