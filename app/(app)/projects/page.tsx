1560652366b48ff9a9b982816bfa8d8f1c775cb1
AkkalDhami <dhamiakkal21@gmail.com>
2026-03-29 09:48:31 +0545

    fix: fix the padding from top

--- app/(app)/projects/page.tsx
+++ app/(app)/projects/page.tsx
@@ -12,7 +12,7 @@ export default function Page() {
   return (
     <div
       className={cn(
-        "border-edge mx-auto max-w-4xl border-x pt-18",
+        "border-edge mx-auto max-w-4xl border-x pt-16",
         "bg-[radial-gradient(35%_128px_at_0%_0%,--theme(--color-foreground/.05),transparent)] dark:bg-[radial-gradient(35%_128px_at_0%_0%,--theme(--color-foreground/.08),transparent),radial-gradient(35%_128px_at_100%_0%,--theme(--color-foreground/.08),transparent)]"
       )}>
       <ProjectsSection details={false} />
